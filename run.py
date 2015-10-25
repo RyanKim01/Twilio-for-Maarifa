d6db56d78689135ac6486a342d94aa5ad860c892
RyanKim01 <kyhekm@hotmail.com>
2015-10-24 18:07:58 -0700

    Implemented new ways of replying a text

--- run.py
+++ run.py
@@ -3,9 +3,20 @@ import twilio.twiml
 
 app = Flask(__name__)
 
+# Try adding your own number to this list!
+callers = {
+    "+14155686292": "Curious Unathi",
+}
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
+
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", where are your home girls?!"
+    else:
+        message = "Monkey, thanks for the message!"
 
     resp = twilio.twiml.Response()
     resp.message("Hello, Mobile Monkey")
