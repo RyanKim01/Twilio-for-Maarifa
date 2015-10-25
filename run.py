e0e2262456d5f717b631dca052e030637308b1da
RyanKim01 <kyhekm@hotmail.com>
2015-10-25 00:28:48 -0700

    Implemented hardcoded math portion

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import requests
 
 app = Flask(__name__)
 
@@ -8,27 +9,44 @@ callers = {
     "+14155686292": "Curious Unathi",
     "+13102546839": "Ryan"
 }
+subjects = ["math", "science", "english"]
+math_grades = ["Grade 1", "Grade 2", "Grade 3", "Grade 4"]
 
-# subjects = [
-#     "math", "science", "english"
-# ]
-#
-# math_grades = [
-#     "Grade 1", "Grade 2", "Grade 3", "Grade 4"
-# ]
+# def request_data(subject, lesson_id):
+#     r = requests.get('http://9722c1ea.ngrok.io/api/lesson')
+#     data = r.json()
+#     for lessons in data['objects']:
+#         if subject in lessons.values():
+#             if lessons["lesson_id"] == lesson_id:
+#                 return lessons["lesson_content"]
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond and greet the caller by name."""
+def answering_algorithm():
+    resp = twilio.twiml.Response()
+    body = request.values.get('Body', None).lower()
+    from_number = request.values.get('From', None)
+    users = requests.get('http://9722c1ea.ngrok.io/api/user')
+    user_data = users.json()
 
     from_number = request.values.get('From', None)
     if from_number in callers:
-        message = callers[from_number] + ", thanks for the message!"
+        message = callers[from_number] + ", which subject are you looking for?"
+        + "We have Math, Science, and English"
+        resp.message(message)
+        if body == "math":
+            message = "Which grade are you looking for?"
+            + "We have grade 1 through grade 6."
+            resp.message(message)
+                if body == "grade 1":
+                    message = "Which content do you want to access?"
+                    + "We have addition, subtraction, and prime number."
+                    resp.message(message)
     else:
-        message = "Monkey, thanks for the message!"
+        message = "Welcome to Maarifa. Which subject are you looking for?"
+        + "We have Math, Science, and English"
+        resp.message(message)
 
-    resp = twilio.twiml.Response()
-    resp.message(message)
+
 
     return str(resp)
 
